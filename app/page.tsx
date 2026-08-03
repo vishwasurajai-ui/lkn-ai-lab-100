0a4f28d05337be25e16731126c96f7dd21105224
Vishwa Suraj <vishwasuraj.ai@gmail.com>
2026-08-03 14:48:01 -0400

    your message

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            HELLO WORLD ABC
+            HELLO WORLD.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Build and deploy your first AI-powered app.
